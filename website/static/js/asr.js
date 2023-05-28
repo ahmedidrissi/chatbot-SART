29c3cb20f24533c7af6623aaf9a417f11e876b6b
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-05-28 22:16:52 +0100

    add french option

--- website/static/js/asr.js
+++ website/static/js/asr.js
@@ -1,58 +1,63 @@
-try {
-    const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
-    const recognition = new SpeechRecognition();
-    
-    //let final_transcript = "";
+const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
 
-    recognition.continuous = false;
-    recognition.interimResults = false;
-    recognition.lang = 'en-US';
+const synthesis = window.speechSynthesis;
+const utterance = new SpeechSynthesisUtterance();
+synthesis.cancel();
 
-    function changeLanguage(lang) {
-        recognition.lang = lang;
-    };
+recognition.continuous = false;
+recognition.interimResults = false;
+recognition.lang = 'en-US';
 
-    document.querySelector("#mic").onclick = () => {
-        const mic = document.querySelector('#mic').classList;
-        if (mic.contains("mic-off")) {
-            recognition.start();
-        }
-    };
+function changeLanguage(lang) {
+    recognition.lang = lang;
+    let voices = speechSynthesis.getVoices();
 
-    recognition.onstart = () => {
-        const mic = document.querySelector('#mic').classList;
-        const mic_icon = document.querySelector('#mic-icon').classList;
-        mic.remove("mic-off");
-        mic_icon.remove("bi-mic-mute-fill");
-        mic.add("mic-on");
-        mic_icon.add("bi-mic-fill");
-        document.querySelector("#input").placeholder = "Listening...";
-    };
-    recognition.onerror = () => {
-        alert("Speech Recognition Error !");
-        console.log("Speech Recognition Error");
-    };
-    recognition.onend = () => {
-        document.querySelector("#input").placeholder = "Enter your message...";
-        const mic = document.querySelector('#mic').classList;
-        const mic_icon = document.querySelector('#mic-icon').classList;
-        mic.remove("mic-on");
-        mic_icon.remove("bi-mic-fill");
-        mic.add("mic-off");
-        mic_icon.add("bi-mic-mute-fill");
-        console.log("Speech Recognition Ended");
-    };
+    if (lang === "en-US") {
+        utterance.voice = voices[1];
+    } else {
+        utterance.voice = voices[4];
+    }        
+};
 
-    recognition.onresult = (event) => {
-        document.querySelector("#input").value = event.results[0][0].transcript;
-        document.querySelector('#send').click();
-    }; 
+document.querySelector("#mic").onclick = () => {
+    const mic = document.querySelector('#mic').classList;
+    if (mic.contains("mic-off")) {
+        recognition.start();
+    }
+};
 
-    recognition.onspeechend = () => {
-        recognition.stop();
-    };
+recognition.onstart = () => {
+    const mic = document.querySelector('#mic').classList;
+    const mic_icon = document.querySelector('#mic-icon').classList;
+    mic.remove("mic-off");
+    mic_icon.remove("bi-mic-mute-fill");
+    mic.add("mic-on");
+    mic_icon.add("bi-mic-fill");
+    document.querySelector("#input").placeholder = "Listening...";
+};
 
-} catch (error) {
-    alert("Speech Recognition is not available in your brower :(");
-    console.log("Speech Recognition Not Available");
-}
+recognition.onerror = () => {
+    alert("Speech Recognition Error !");
+    console.log("Speech Recognition Error");
+};
+
+recognition.onend = () => {
+    document.querySelector("#input").placeholder = "Enter your message...";
+    const mic = document.querySelector('#mic').classList;
+    const mic_icon = document.querySelector('#mic-icon').classList;
+    mic.remove("mic-on");
+    mic_icon.remove("bi-mic-fill");
+    mic.add("mic-off");
+    mic_icon.add("bi-mic-mute-fill");
+    console.log("Speech Recognition Ended");
+};
+
+recognition.onresult = (event) => {
+    document.querySelector("#input").value = event.results[0][0].transcript;
+    document.querySelector('#send').click();
+}; 
+
+recognition.onspeechend = () => {
+    recognition.stop();
+};
