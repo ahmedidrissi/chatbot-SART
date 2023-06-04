ef92cca33fa541edd7329d521e1000ee8c3d5b4a
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-06-04 18:42:15 +0100

    add googletrans

--- website/views.py
+++ website/views.py
@@ -1,6 +1,17 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 views = Blueprint('views', __name__)
 
+from googletrans import Translator
+translator = Translator()
+
 @views.route('/', methods=['GET', 'POST'])
+@views.route('/index', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        data = request.json
+        text = data['userMessage']
+        lang = data['lang']
+        translatedText = translator.translate(text, dest=lang)
+        
+        return jsonify({"translatedMessage": translatedText.text})
     return render_template("index.html")
